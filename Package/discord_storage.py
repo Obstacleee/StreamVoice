78f60cfde63e0b737829298ce42a899da1f7616c
Obstacleee <147484382+Obstacleee@users.noreply.github.com>
2024-04-16 09:34:43 +0200

    a

--- Package/discord_storage.py
+++ Package/discord_storage.py
@@ -1,11 +1,23 @@
 import requests
-from discord import SyncWebhook # Import SyncWebhook
-import discord
+from discord import SyncWebhook, File
+import time
+
 webhook_link = 'https://discord.com/api/webhooks/1229419212444008478/QrQic988XoqowRULvfX5V7EVEetI3WL0tQoJ5XO923hMlF8CV7zoCNlV1dsd1tf28M_8'
-def send_files(file_path):
-    webhook = SyncWebhook.from_url(webhook_link) # Initializing webhook
 
-    with open(file=file_path, mode='rb') as f:
-        my_file = discord.File(f)
+def send_and_get_file_link(file_path):
+    webhook = SyncWebhook.from_url(webhook_link)
 
-    webhook.send(username='RSS to Audio', file=my_file)
+    try:
+        with open(file_path, 'rb') as f:
+            my_file = File(f)
+            response = webhook.send(file=my_file, wait=True)
+    except Exception as e:
+        print(f'Erreur lors de l\'envoi du fichier : {str(e)}')
+        return None
+
+    if response and response.attachments:
+        file_url = response.attachments[0].url  # Obtenez l'URL du premier attachement
+        return file_url
+    else:
+        print('Aucune réponse du serveur Discord ou pas d\'attachements dans la réponse.')
+        return None
